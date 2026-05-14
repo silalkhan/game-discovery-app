463a6d718f6e203ed913ca1d6906babd65d972db
Silal Khan <silalnoor999@gmail.com>
2026-05-14 14:27:51 +0500

    Build a responsvie layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <>
-      <h1>HelloWorld</h1>
-    </>
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-[200px_1fr] grid-rows-[auto_1fr]">
+      {/* Navbar */}
+      <nav className="bg-orange-400 p-4 lg:col-span-2">Nav</nav>
+
+      {/* Sidebar */}
+      <aside className="hidden lg:block bg-gray-300 p-4">Aside</aside>
+
+      {/* Main Content */}
+      <main className="bg-green-300 p-4">Main</main>
+    </div>
   );
 }
 
