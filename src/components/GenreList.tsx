2801cd31d5a27bb10708a08f88d555da1e49587d
Silal Khan <silalnoor999@gmail.com>
2026-05-22 02:57:40 +0500

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,45 @@
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <ul className="space-y-3">
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <li
+          key={genre.id}
+          className="
+            flex
+            items-center
+            gap-3
+            cursor-pointer
+            hover:bg-gray-100
+            dark:hover:bg-gray-700
+            p-2
+            rounded-lg
+            transition-colors
+          "
+        >
+          <img
+            src={getCroppedImageUrl(genre.image_background)}
+            alt={genre.name}
+            className="
+              w-8
+              h-8
+              rounded-lg
+              object-cover
+            "
+          />
+          <p
+            className="
+              text-gray-800
+              dark:text-gray-100
+              font-medium
+            "
+          >
+            {genre.name}
+          </p>
+        </li>
       ))}
     </ul>
   );
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -10,31 +10,33 @@ interface FetchResponse<T> {
   results: T [];
 }
 //so here we use Genre type parameter <T> instead of <Genre[]>
-const useData =<T> (endPoint:string) => {
+const useData = <T>(endPoint: string) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
     const controller = new AbortController();
-    const signal = controller.signal;
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setLoading(true);
+
     apiClient
-      .get<FetchResponse<T>>(endPoint, { signal })
+      .get<FetchResponse<T>>(endPoint, {
+        signal: controller.signal,
+      })
       .then((res) => {
         setData(res.data.results);
         setLoading(false);
       })
-
       .catch((err) => {
         if (err instanceof CanceledError) return;
 
         setError(err.message);
         setLoading(false);
       });
+
     return () => controller.abort();
-  }, []);
+  }, [endPoint]);
+
   return { data, error, isLoading };
 };
-
 export default useData;
+
